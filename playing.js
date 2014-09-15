fc80c1b7f0f29e1fde5fc070016e70c4daa29a72
Anthony F <afrehner.work@gmail.com>
2014-09-15 16:41:23 -0600

    I have a basic layout working, but I'm stuck on getting a new item saved to the items array.

--- playing.js
+++ playing.js
@@ -1,3 +1,18 @@
 (function() {
 	var app = angular.module('playingApp', [])
+
+	var items = [{'label':'Ham', 'number': 21}, {'label':'Cabbage', 'number': 88}]
+
+	app.controller('ItemsController', function(){
+		this.allItems = items
+	})
+
+	app.controller('D3InfoController', function(){
+		this.item = {}
+
+		this.addItem = function(allItems){
+			allItems.push(this.item)
+			this.item = {}
+		}
+	})
 })()

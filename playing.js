1cdf674f8e195283454d20df9b7743fa68c73b57
Anthony F <afrehner.work@gmail.com>
2014-09-17 16:40:54 -0600

    I now have ng repeat working with new items added, thanks to a better understanding of . I just hope I'm using it correctly.

--- playing.js
+++ playing.js
@@ -3,16 +3,13 @@
 
 	var items = [{'label':'Ham', 'number': 21}, {'label':'Cabbage', 'number': 88}]
 
-	app.controller('ItemsController', function(){
-		this.allItems = items
-	})
+	app.controller('ItemsController', function($scope){
+		$scope.allItems = items
+		$scope.item = {}
 
-	app.controller('D3InfoController', function(){
-		this.item = {}
-
-		this.addItem = function(allItems){
-			allItems.push(this.item)
-			this.item = {}
+		$scope.addItem = function(){
+			$scope.allItems.push($scope.item)
+			$scope.item = {}
 		}
 	})
 })()

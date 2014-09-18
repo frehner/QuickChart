ac9a2a56563f6d6b8ac9b178b4f2734e8845fb64
Anthony Frehner <af@Anthonys-MacBook-Pro.local>
2014-09-17 22:26:51 -0600

    Added validation to the form, and made it only submittable when it's dirty.  Cleaned up the code as well.

--- playing.js
+++ playing.js
@@ -1,15 +1,20 @@
 (function() {
-	var app = angular.module('playingApp', [])
+	var app = angular.module('playingApp', []);
 
-	var items = [{'label':'Ham', 'number': 21}, {'label':'Cabbage', 'number': 88}]
+	var items = [{'label':'Ham', 'number': 21}, {'label':'Cabbage', 'number': 88}];
 
 	app.controller('ItemsController', function($scope){
-		$scope.allItems = items
-		$scope.item = {}
+		$scope.allItems = items;
+		$scope.item = {};
 
 		$scope.addItem = function(){
-			$scope.allItems.push($scope.item)
-			$scope.item = {}
-		}
-	})
-})()
+			$scope.allItems.push($scope.item);
+
+			//clear the form
+			$scope.item = {};
+
+			//make the form pristine so that the css doesn't apply anymore
+			$scope.addItemForm.$setPristine();
+		};
+	});
+})();

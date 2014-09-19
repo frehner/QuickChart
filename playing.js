63bc2e20c7ba6debc6a399f7546351a731b14f3b
Anthony F <afrehner.work@gmail.com>
2014-09-19 14:34:38 -0600

    I have the graph redrawing every time a new item is added, and also added a scale so that the items are drawn appropriately to their length.

--- playing.js
+++ playing.js
@@ -5,17 +5,27 @@
 
 	var svgwidth = 500;
 	var svgheight = 500;
-	var barheight = 15;
+	var barheight = 45;
 
-	var drawGraphFunc = function(scope, element, attrs){
+	var drawGraphFunc = function(newItems){
+
+		var scale = d3.scale.linear()
+							.domain([1, d3.max(newItems, function(d){
+								return d.number;
+							})])
+							.range([0,svgwidth]);
 				
-		var svg = d3.select(element[0])
-					.append("svg")
-					.attr("width", svgwidth)
-					.attr("height", svgheight);
+		var drawContainer = d3.select("drawing-div")
+
+		drawContainer.selectAll("*")
+						.remove();
+
+		var svg = drawContainer.append("svg")
+								.attr("width", svgwidth)
+								.attr("height", svgheight);
 
 		var group = svg.selectAll("g")
-						.data(items)
+						.data(newItems)
 						.enter()
 						.append("g")
 						.attr("transform", function(d,i){
@@ -24,10 +34,20 @@
 
 		group.append("rect")
 				.attr("width", function(d){
-					return d.number;
+					return scale(d.number);
 				})
 				.attr("height", barheight)
 				.attr("fill", "steelblue");
+
+		group.append("text")
+				.attr("x", function(d){
+					return 5;
+				})
+				.attr("y", barheight/2)
+				.attr("dy", ".35em")
+				.text(function(d){
+					return d.label;
+				});
 	};
 
 	app.controller('ItemsController', function($scope){
@@ -42,14 +62,16 @@
 
 			//make the form pristine so that the css doesn't apply anymore
 			$scope.addItemForm.$setPristine();
+
+			drawGraphFunc($scope.allItems);
 		};
 	});
 
-	//this should draw it the first time
+	//this should draw it the first time with default data
 	app.directive('drawingDiv', function(){
 		return {
 			restrict:'E', 
-			link: drawGraphFunc
+			link: drawGraphFunc(items)
 		};
 	});
 

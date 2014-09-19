dccf1ceb6ef7e87fa640f7f9424f827958edce31
Anthony F <afrehner.work@gmail.com>
2014-09-19 12:59:57 -0600

    D3 is now drawing the objects that already exist in the array, now I need to set a watcher on the array and call the function again when the array is updated... I think.

--- playing.js
+++ playing.js
@@ -3,6 +3,33 @@
 
 	var items = [{'label':'Ham', 'number': 21}, {'label':'Cabbage', 'number': 88}];
 
+	var svgwidth = 500;
+	var svgheight = 500;
+	var barheight = 15;
+
+	var drawGraphFunc = function(scope, element, attrs){
+				
+		var svg = d3.select(element[0])
+					.append("svg")
+					.attr("width", svgwidth)
+					.attr("height", svgheight);
+
+		var group = svg.selectAll("g")
+						.data(items)
+						.enter()
+						.append("g")
+						.attr("transform", function(d,i){
+							return "translate(0, "+i*(barheight+5)+")";
+						});
+
+		group.append("rect")
+				.attr("width", function(d){
+					return d.number;
+				})
+				.attr("height", barheight)
+				.attr("fill", "steelblue");
+	};
+
 	app.controller('ItemsController', function($scope){
 		$scope.allItems = items;
 		$scope.item = {};
@@ -17,4 +44,14 @@
 			$scope.addItemForm.$setPristine();
 		};
 	});
+
+	//this should draw it the first time
+	app.directive('drawingDiv', function(){
+		return {
+			restrict:'E', 
+			link: drawGraphFunc
+		};
+	});
+
+
 })();

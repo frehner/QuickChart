32aa3dd22507825c8ffbbdb17b96cbad74c3494b
Anthony F <afrehner.work@gmail.com>
2014-10-01 16:57:26 -0600

    Working on getting the basics of a vertical bar chart working. It is drawing the graphs correctly, just need to get the text in a way I want it as well.

--- playing.js
+++ playing.js
@@ -3,19 +3,15 @@
 
 	var items = [{'label':'Ham', 'number': 21}, {'label':'Cabbage', 'number': 88}];
 
-	var graphTypes = [{'type':'Horizontal Bar Chart'}];
+	var graphTypes = [{'type':'Horizontal Bar Chart'}, {'type':'Vertical Bar Chart'}];
 
 	var svgwidth = 500;
 	var svgheight = 500;
 	var barheight = 45;
 
-	var drawGraphFunc = function(newItems){
+	var topGraphType = 1;
 
-		var scale = d3.scale.linear()
-							.domain([0, d3.max(newItems, function(d){
-								return d.number;
-							})])
-							.range([0,svgwidth]);
+	var drawGraphFunc = function(newItems){
 
 		var drawContainer = d3.select("drawing-div");
 
@@ -26,7 +22,59 @@
 								.attr("width", svgwidth)
 								.attr("height", svgheight);
 
-		var group = svg.selectAll("g")
+		
+		switch (topGraphType) {
+			case 2:
+				//vertical bar chart
+
+				var scale = d3.scale.linear()
+							.domain([0, d3.max(newItems, function(d){
+								return d.number;
+							})])
+							.range([svgwidth,0]);
+
+				var group = svg.selectAll("g")
+								.data(newItems)
+								.enter()
+								.append("g")
+								.attr("transform", function(d,i) {
+									return "translate("+i*(barheight+5)+", 0)";
+								});
+
+				group.append("rect")
+						.transition()
+						.delay(function(d,i){
+							return (i+1)*500;
+						})
+						.duration(1000)
+						.attr("y", function(d){
+							return scale(d.number);
+						})
+						.attr("height", function(d){
+							return svgheight - scale(d.number);
+						})
+						.attr("width", barheight)
+						.attr("fill", "steelblue");
+
+				group.append("text")
+						.attr("x", barheight/2)
+						.attr("y", 5)
+						.attr("dx", ".35em")
+						.text(function(d){
+							return d.label;
+						});
+				break;
+
+			default:
+				//horizontal bar chart
+
+				var scale = d3.scale.linear()
+							.domain([0, d3.max(newItems, function(d){
+								return d.number;
+							})])
+							.range([0,svgwidth]);
+
+				var group = svg.selectAll("g")
 						.data(newItems)
 						.enter()
 						.append("g")
@@ -34,27 +82,30 @@
 							return "translate(0, "+i*(barheight+5)+")";
 						});
 
-		group.append("rect")
-				.transition()
-				.delay(function(d, i){
-					return (i+1)*500;
-				})
-				.duration(1000)
-				.attr("height", barheight)
-				.attr("fill", "steelblue")
-				.attr("width", function(d){
-					return scale(d.number);
-				});
+				group.append("rect")
+						.transition()
+						.delay(function(d, i){
+							return (i+1)*500;
+						})
+						.duration(1000)
+						.attr("height", barheight)
+						.attr("fill", "steelblue")
+						.attr("width", function(d){
+							return scale(d.number);
+						});
 
-		group.append("text")
-				.attr("x", function(d){
-					return 5;
-				})
-				.attr("y", barheight/2)
-				.attr("dy", ".35em")
-				.text(function(d){
-					return d.label;
-				});
+				group.append("text")
+						.attr("x", function(d){
+							return 5;
+						})
+						.attr("y", barheight/2)
+						.attr("dy", ".35em")
+						.text(function(d){
+							return d.label;
+						});
+				break;
+		}
+		
 	};
 
 	app.controller('ItemsController', function($scope){
@@ -89,7 +140,8 @@
 
 		//watchers on vars
 		$scope.$watchCollection('allItems', drawGraphFunc);
-		$scope.$watch('graphType', function(newGraphType) {
+		$scope.$watch('graphType', function() {
+			topGraphType = $scope.graphType;
 			drawGraphFunc($scope.allItems);
 		});
 	});
